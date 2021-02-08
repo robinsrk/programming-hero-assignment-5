40b75fd5114aa0730b3fb569d54dcc4eca5b4ebb
robinsrk <robinsrk3@gmail.com>
2021-02-08 16:13:24 +0600

    update

--- app.js
+++ app.js
@@ -21,14 +21,20 @@ function randomMeals(meals) {
 //search meals
 document.getElementById('search-btn').addEventListener("click", function () {
     const mealName = document.getElementById("search-box").value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
-    .then(res => res.json())
+    console.log(mealName);
+    if (mealName) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+            .then(res => res.json())
 
-    // call function to show the information of meal
-    .then(data => showMealInfo(data.meals[0],mealName))
+            // call function to show the information of meal
+            .then(data => showMealInfo(data.meals[0], mealName))
 
-    // show error if meal not found
-    .catch(error => alert("Item not found"))
+            // show error if meal not found
+            .catch(error => alert("Item not found"))
+    }
+    else {
+        alert("Enter a meal name to search");
+    }
 })
 
 // details of each meal
@@ -38,16 +44,16 @@ function mealInfo() {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
         .then(res => res.json())
         .then(data => showMealInfo(data.meals[0], mealName, mealDiv))
- }
+}
 
- //home button to clear the meal information
-document.getElementById('home').addEventListener("click",function(){
+//home button to clear the meal information
+document.getElementById('home').addEventListener("click", function () {
     const mealInfo = document.getElementById('meal-details');
     mealInfo.style.display = 'none';
 })
 
 // show meal information in separate div
-function showMealInfo(meal, mealName){
+function showMealInfo(meal, mealName) {
     const mealDiv = document.getElementById('meal-details');
     mealDiv.style.display = "block";
     mealDiv.style.backgroundColor = "orange";
@@ -56,12 +62,13 @@ function showMealInfo(meal, mealName){
     <br>
     <br>
     <h5>Ingredients</h5>
-    <ul>
-        <li>${meal.strIngredient1}hello</li>
-        <li>${meal.strIngredient2}</li>
-        <li>${meal.strIngredient3}</li>
-        <li>${meal.strIngredient4}</li>
-        <li>${meal.strIngredient5}</li>
-    </ul>
+      <ul>
+         <li>${meal.strIngredient1}hello</li>
+         <li>${meal.strIngredient2}</li>
+         <li>${meal.strIngredient3}</li>
+         <li>${meal.strIngredient4}</li>
+         <li>${meal.strIngredient5}</li>
+     </ul>
     `
+
 }
